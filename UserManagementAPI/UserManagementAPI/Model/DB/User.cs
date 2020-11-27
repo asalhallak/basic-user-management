0f367e23f089bac178147921e588ffbcc400a5dc
Abdullah S. Al-Hallak <alhallak@capella.io>
2020-11-27 19:07:13 +0200

    Adding domain and data access project

--- UserManagementAPI/UserManagementAPI/Model/DB/User.cs
+++ UserManagementAPI/UserManagementAPI/Model/DB/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace UserManagementAPI.Model.DB
 {
-    public class Users
+    public class User
     {
         public int Id { get; set; }
         public string LoginName { get; set; }
---
+++ UserManagementAPI/UserManagementAPI/Model/DB/UsersManagement.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagementAPI.Model.DB
+{
+    public class UsersManagement : DbContext
+    {
+        public UsersManagement(DbContextOptions<UsersManagement> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Address>().ToTable("Address");
+        }
+
+    }
+}
